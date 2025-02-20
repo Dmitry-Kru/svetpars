575e73f2de4cdb87476fd2f84dd619fa0781a042
Dmitry-Kru <dukeingm@gmail.com>
2025-02-20 12:49:39 +0300

    final

--- svetscrapy/svetscrapy/spiders/svetpars.py
+++ svetscrapy/svetscrapy/spiders/svetpars.py
@@ -3,8 +3,20 @@ import scrapy
 
 class SvetparsSpider(scrapy.Spider):
     name = "svetpars"
-    allowed_domains = ["divan.ru"]
-    start_urls = ["https://divan.ru"]
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        pass
+        # Создаём переменную, в которую будет сохраняться информация
+        svet = response.css('div._Ud0k')
+        # Настраиваем работу с каждым отдельным светильником в списке
+        for item in svet:
+            yield {
+                # Создаём словарик названий, используем поиск по диву, а внутри дива — по тегу span
+                'name': item.css('div.lsooF span::text').get(),
+                # Создаём словарик цен, используем поиск по диву, а внутри дива — по тегу span
+                'price': item.css('div.pY3d2 span::text').get(),
+                # Создаём словарик ссылок, используем поиск по тегу "a", а внутри тега — по атрибуту
+                # Атрибуты — это настройки тегов
+                'url': item.css('a').attrib['href']
+            }
